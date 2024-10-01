7ca7edb0b3255012c0ab3f7c3ead6ec528485983
ray__s <Rayy_Bush@proton.me>
2024-10-01 09:19:19 +0300

    Задания 1-67

--- w1-67.js
+++ w1-67.js
@@ -1,23 +1,3 @@
-let a = 10;
-let b = 2;
-
-alert("Сумма: " + (a + b));    
-alert("Произведение: " + (a * b)); 
-alert("Частное: " + (a / b));    
-let c = 10;
-let d = 5;
-
-let result = c + d;
-alert("Результат: " + result);
-let e = 1;
-let f = 2;
-let g = 3;
-
-alert("Сумма: " + (e+f+g));
-let h = 10;
-let i = 5;
-
-let j = h - i;
-let k = 7;
-let result1 = j + k;
-alert("Результат: " + result1); 
+let a = 13;
+let b= 5;
+alert(a%b);
