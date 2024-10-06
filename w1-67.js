85e42565d51cb0a06a266c2cb13efc5165f8f406
ray__s <Rayy_Bush@proton.me>
2024-10-06 03:08:36 +0300

    Задания 1-67

--- w1-67.js
+++ w1-67.js
@@ -1,4 +1,5 @@
-let str1 = 'xxx';
-let str2 = 'yyy';
-let txt  = aaa  `${str1}  bbb ${str2} ccc`;
-alert(txt);
+let num1 = Number(prompt('Введите первое число'));
+let num2 = Number(prompt('Введите первое число'));
+
+
+alert(num1 + num2); 
