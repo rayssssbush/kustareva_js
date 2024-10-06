73028bd3995aa2b32c1c26029f75255c6834d38d
ray__s <Rayy_Bush@proton.me>
2024-10-06 12:53:52 +0300

    Задания 1-67

--- w1-67.js
+++ w1-67.js
@@ -1,5 +1,11 @@
-let num1 = Number(prompt('Введите первое число'));
-let num2 = Number(prompt('Введите первое число'));
-
-
-alert(num1 + num2); 
+let a = "abcdefg"
+m=1024*1024;
+document.write(a);
+document.write('<br>');
+document.write('<i>'+ a + '<\i><br>');
+document.write('1<br>');
+document.write('2<br>');
+document.write('3<br>');
+document.write('4<br>');
+document.write('5<br>');
+document.write('В террабайте '+String(m)+ ' килобайт')
