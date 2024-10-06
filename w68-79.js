a5cd64cb3daf536edbf8324c04cedd6e0d4db7d5
ray__s <Rayy_Bush@proton.me>
2024-10-07 02:55:36 +0300

    Задания 68-79

--- w68-79.js
+++ w68-79.js
@@ -1,5 +1,9 @@
 let arr = [1,2,3,4,5];
 let arr1 = ['a', 'b', 'c'];
+let arr2=[];
+let arr3 = ['a', 'b', 'c', 'd', 'e'];
+let key1 = 1;
+let key2 = 2;
 console.log(arr);
 //Вывод массиав при помощи цикла в консоль
 for (let i = 0; i < arr.length; i++) {
@@ -14,3 +18,10 @@ for (let i = 0; i < arr1.length; i++) {
     arr1[i] ++;
 }
 console.log(arr1);
+for (let i = 1; i<6;i++){
+    arr2.push(i);
+}
+console.log(arr2)
+console.log(arr[key1]+arr[key2])
+console.log(delete arr3[2])
+console.log(arr[arr.length]);
