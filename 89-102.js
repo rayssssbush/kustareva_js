3411393c6cbe541bcfb61939a52563734be79c72
ray__s <Rayy_Bush@proton.me>
2024-10-08 08:45:49 +0300

    Задания 80-103

--- 89-102.js
+++ 89-102.js
@@ -1,2 +1,4 @@
-let obj = {1: 'a', 2: 'b', 3: 'c', 4:'e', 5: 'f', 6: 'g', 7:'h'};
-console.log(obj);
+let user = {name: "Иван",surname: "Петров",patronymic: "Сергеевич"
+};
+
+console.log(user.surname + " " + user.name + " " + user.patronymic);
