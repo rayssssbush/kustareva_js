1f4a27245832f4ef10b105673493d3e5fcc53bd0
ray__s <Rayy_Bush@proton.me>
2024-10-21 00:59:27 +0300

    Задания 104-142

--- 104-142.js
+++ 104-142.js
@@ -1,1 +1,11 @@
-if (test > 0) console.log('+++');  else console.log('---');
+let num = 12;
+
+if ((1<=num<=10)) {
+	console.log('first');
+} else if (10<=num<=20) {
+	console.log('second');
+} else if (20<=num<=31) {
+	console.log('third');
+} else {
+	console.log('неверное значение переменной num');
+}
