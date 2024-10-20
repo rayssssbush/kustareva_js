c49a3739246f6b8ef35fb4feafce5743a64a4684
ray__s <Rayy_Bush@proton.me>
2024-10-21 01:42:54 +0300

    Задания 104-142

--- 104-142.js
+++ 104-142.js
@@ -1,17 +1,13 @@
-let num1 = 25;
-let str_num = String(num1);
-let b = 0;
-
-if (10 <= num1 && num1 <= 99) {
-  for (let i = 0; i < str_num.length; i++) {
-    b += parseInt(str_num[i]);
-  }
-
-  if (b <= 9) {
-    console.log("Сумма цифр однозначна: " + b);
-  } else {
-    console.log("Сумма цифр двухзначна: " + b);
-  }
-} else {
-  console.log("Число не попадает в диапазон от 10 до 99.");
+let lang = 'ru';
+switch(lang){
+case "ru":
+	console.log('рус');
+	break;
+ case "en":
+	console.log('анг');
+	break;
+case "de":
+	console.log('нем');
+default:
+	console.log('язык не поддерживается');
 }
