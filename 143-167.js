8c15745891dab7f584b3d833247298c69839f290
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:34:05 +0300

    Задания 143-167

--- 143-167.js
+++ 143-167.js
@@ -1,7 +1,7 @@
-let sum = 0;
+let product = 1;
 
-for (let i = 2; i <= 100; i += 2) {
-    sum += i;
+for (let i = 1; i <= 20; i++) {
+    product *= i;
 }
 
-console.log(sum);
+console.log(product);
