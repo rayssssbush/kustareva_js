72646be00360a7d81013bea26cb7e9a4dae8f3b5
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:43:46 +0300

    Задания 143-167

--- 143-167.js
+++ 143-167.js
@@ -1,9 +1,10 @@
-let arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-let arr2 = [1, 2, 3, 4, 5, 6, 7];
-let daysObj = {};
+let obj = {a: 1, b: 2, c: 3, d: 4, e: 5};
+let evenObj = {};
 
-for (let i = 0; i < arr1.length; i++) {
-    daysObj[arr1[i]] = arr2[i];
+for (let key in obj) {
+    if (obj[key] % 2 === 0) {
+        evenObj[key] = obj[key];
+    }
 }
 
-console.log(daysObj);
+console.log(evenObj);
