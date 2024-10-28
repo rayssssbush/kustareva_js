791e103097d149432f2f89a67403682868fbe462
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:56:57 +0300

    Задания 143-167

--- 168-183.js
+++ 168-183.js
@@ -1,11 +1,8 @@
-let obj = {1: 125, 2: 225,3: 128, 4: 356,5: 145,6: 281,7: 452};
-let result = [];
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let obj = {};
 
-for (let key in obj) {
-    let value = obj[key].toString();
-    if (value[0] === '1' || value[0] === '2') {
-        result.push(obj[key]);
-    }
+for (let i = 0; i < arr.length; i++) {
+    obj[arr[i]] = i + 1;
 }
 
-console.log(result);
+console.log(obj);
