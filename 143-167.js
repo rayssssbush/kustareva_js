7f2ddcf81a43abf443e5ac4f3b5f1b7719f2cb70
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:43:33 +0300

    Задания 143-167

--- 143-167.js
+++ 143-167.js
@@ -1,7 +1,9 @@
-let arr = [];
+let arr1 = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+let arr2 = [1, 2, 3, 4, 5, 6, 7];
+let daysObj = {};
 
-for (let i = 0; i < 10; i++) {
-    arr.push('x');
+for (let i = 0; i < arr1.length; i++) {
+    daysObj[arr1[i]] = arr2[i];
 }
 
-console.log(arr);
+console.log(daysObj);
