18c9407dd76c556edcc0c2ad209a9e24526bce7d
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:36:43 +0300

    Задания 143-167

--- 143-167.js
+++ 143-167.js
@@ -1,5 +1,8 @@
 for (let i = 10; i <= 1000; i++) {
-    if (String(i)[0] == '1') {
+    let str = String(i);
+    let sumOfFirstTwoDigits = Number(str[0]) + Number(str[1]);
+    
+    if (sumOfFirstTwoDigits === 5) {
         console.log(i);
     }
 }
