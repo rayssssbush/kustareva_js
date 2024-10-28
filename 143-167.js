c3b78cdec035baea56e35dd2b3b00efaa10a7f23
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:33:02 +0300

    Задания 143-167

--- 143-167.js
+++ 143-167.js
@@ -1,7 +1,7 @@
-let arr = [2, 5, 9, 15, 1, 4];
+let obj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 3 && arr[i] < 10) {
-        console.log(arr[i]);
+for (let key in obj) {
+    if (obj[key] % 2 !== 0) {
+        console.log(obj[key]);
     }
 }
