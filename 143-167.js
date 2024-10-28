d770c19cfc74d3b1937fc64858c3efc8a2d6d0e7
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:42:36 +0300

    Задания 143-167

--- 143-167.js
+++ 143-167.js
@@ -1,10 +1,7 @@
-let arr = [-2, 3, -5, 8, -1, 4];
-let positiveArr = [];
+let arr = [];
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-        positiveArr.push(arr[i]);
-    }
+for (let i = 0; i < 10; i++) {
+    arr.push('x');
 }
 
-console.log(positiveArr);
+console.log(arr);
