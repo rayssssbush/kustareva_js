c929fccb7ada27c40c63062d447bd8f108b5350e
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:38:28 +0300

    Задания 143-167

--- 143-167.js
+++ 143-167.js
@@ -1,8 +1,11 @@
-let arr = [2, 5, 0, 3, 1, 4];
+let arr = [2, 5, 9, -3, 1, 4];
+let sum = 0;
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
+    if (arr[i] < 0) {
         break;
     }
-    console.log(arr[i]);
+    sum += arr[i];
 }
+
+console.log(sum);
