ac79fb0b055521028e6b1e85bbdf4c719a3cd0c5
ray__s <Rayy_Bush@proton.me>
2024-10-29 00:55:06 +0300

    Задания 143-167

---
+++ 168-183.js
@@ -0,0 +1,11 @@
+let obj = { 'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5 };
+let keys = [];
+let values = [];
+
+for (let key in obj) {
+    keys.push(key);
+    values.push(obj[key]);
+}
+
+console.log(keys);
+console.log(values);
