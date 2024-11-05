5699f92469648d66f382f5bb0325f2eb1f37a548
ray__s <Rayy_Bush@proton.me>
2024-11-05 12:06:47 +0300

    Задания 168-183

--- 168-183.js
+++ 168-183.js
@@ -1,8 +1,15 @@
-let arr = ['a', 'b', 'c', 'd', 'e'];
-let obj = {};
+let arr = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+    ['j', 'k', 'l'],
+];
 
-for (let i = 0; i < arr.length; i++) {
-    obj[arr[i]] = i + 1;
-}
+let elementsToShow = [
+    arr[3][2], 
+    arr[1][1], 
+    arr[2][0], 
+    arr[0][0]  
+];
 
-console.log(obj);
+console.log(elementsToShow);
