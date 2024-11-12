6e6c562866f1f0e544f8aa887de6900e4d28675e
ray__s <Rayy_Bush@proton.me>
2024-11-12 12:45:11 +0300

    Задания 184-199

--- 184-199.js
+++ 184-199.js
@@ -1,5 +1,4 @@
-let randomNumbers = [];
-for (let i = 0; i < 10; i++) {
-    randomNumbers.push(Math.floor(Math.random() * 100) + 1);
-}
-console.log(randomNumbers);
+let a = 5;
+let b = 10;
+let difference = Math.abs(a - b);
+console.log("Модуль разности:", difference);
