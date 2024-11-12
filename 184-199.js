02d800a9abcb4558ebcc800483a3ad1e98737ee6
ray__s <Rayy_Bush@proton.me>
2024-11-12 12:42:47 +0300

    Задания 184-199

--- 184-199.js
+++ 184-199.js
@@ -1,6 +1,7 @@
-let n = [4, 2, 5, 19, 13, 0, 10];
-let m = 0;
-for (let i = 0; i < n.length; i++) {
-    m += Math.pow(n[i], 3);
-}
-console.log(Math.sqrt(m));
+let sqrtResult = Math.sqrt(587);
+let roundedResults = {
+    floor: Math.floor(sqrtResult),
+    ceil: Math.ceil(sqrtResult)
+};
+
+console.log(roundedResults);
