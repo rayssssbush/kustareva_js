77a560dc80c8ffcb340f5f15e93785657a4ec012
ray__s <Rayy_Bush@proton.me>
2024-11-12 12:44:48 +0300

    Задания 184-199

--- 184-199.js
+++ 184-199.js
@@ -1,7 +1,5 @@
-let numbers = [4, -2, 5, 19, -130, 0, 10];
-
-let min = Math.min(...numbers);
-let max = Math.max(...numbers);
-
-console.log("Минимальное число:", min);
-console.log("Максимальное число:", max);
+let randomNumbers = [];
+for (let i = 0; i < 10; i++) {
+    randomNumbers.push(Math.floor(Math.random() * 100) + 1);
+}
+console.log(randomNumbers);
