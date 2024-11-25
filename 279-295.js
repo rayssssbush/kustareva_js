93243c9473b708221f2a7ab317291c128661440a
ray__s <Rayy_Bush@proton.me>
2024-11-26 01:04:09 +0300

    Задания 279-295

--- 279-295.js
+++ 279-295.js
@@ -1,6 +1,7 @@
-const arr = [1, 2, 3, 4, 5];
-let sum = 0;
-arr.forEach(item => {
-  sum += item ** 2;
-});
-console.log(sum); 
+const arr = [1, -2, 3, 12, 7, 0, 9, 11];
+const filteredArr = arr.filter(item => item > 0 && item < 10);
+
+console.log(filteredArr);
+const filteredArr2 = arr.filter((item, index) => item * (index + 1) < 30);
+
+console.log(filteredArr2);
