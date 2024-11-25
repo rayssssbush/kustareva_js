78e12ae6eb8c170d10c0bbc9fe140ccd8cec1163
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:42:55 +0300

    Задания 223-239

--- 240-256.js
+++ 240-256.js
@@ -1,7 +1,10 @@
-function test(func) {
-    return func([1, 2, 3, 4, 5]);
+function square(num) {
+    return num ** 2;
 }
-const result = test(function(arr) {
-    return arr.map(num => num ** 3);
-});
-console.log(result);
+function cube(num) {
+    return num ** 3;
+}
+function func(a, b) {
+    return square(a) + cube(b);
+}
+console.log(func(2, 3));
