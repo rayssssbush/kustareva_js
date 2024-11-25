2f22758b7ef9249317a8a304efc5d95d6162d72a
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:54:33 +0300

    Задания 257-278

--- 257-278.js
+++ 257-278.js
@@ -1,11 +1,14 @@
-function printPrimitives(obj) {
-    for (let key in obj) {
-        if (typeof obj[key] === 'object' && obj[key] !== null) {
-            printPrimitives(obj[key]);
+function flattenArray(arr) {
+    let result = [];
+    arr.forEach(item => {
+        if (Array.isArray(item)) {
+            result = result.concat(flattenArray(item));
         } else {
-            console.log(obj[key]);
+            result.push(item);
         }
-    }
+    });
+    return result;
 }
-const obj = {a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}}; 
-printPrimitives(obj);
+
+const arr = [1, [2, 7, 8], [3, 4, [5, [6, 7]]]];
+console.log(flattenArray(arr));
