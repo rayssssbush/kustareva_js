0fddaf0a65f4c634cd0a14475e914853ef08edfe
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:55:27 +0300

    Задания 257-278

--- 257-278.js
+++ 257-278.js
@@ -1,14 +1,13 @@
-function flattenArray(arr) {
-    let result = [];
-    arr.forEach(item => {
-        if (Array.isArray(item)) {
-            result = result.concat(flattenArray(item));
+function sumObjectValues(obj) {
+    let sum = 0;
+    for (let key in obj) {
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+            sum += sumObjectValues(obj[key]);
         } else {
-            result.push(item);
-        }
-    });
-    return result;
-}
+            sum += obj[key];
+    }
+    return sum;
+}}
 
-const arr = [1, [2, 7, 8], [3, 4, [5, [6, 7]]]];
-console.log(flattenArray(arr));
+const obj = {a: 1, b: {c: 2, d: 3, e: 4}, f: {g: 5, j: 6, k: {l: 7, m: {n: 8, o: 9}}}}; 
+console.log(sumObjectValues(obj));
