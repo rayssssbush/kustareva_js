abfa5ab8ac96e0b75cc53e9e19139699a91ceeef
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:57:53 +0300

    Задания 257-278

--- 257-278.js
+++ 257-278.js
@@ -1,12 +1,3 @@
-function squareArrayElements(arr) {
-    return arr.map(item => {
-        if (Array.isArray(item)) {
-            return squareArrayElements(item);
-        } else {
-            return item * item;
-        }
-    });
-}
-
-const arr = [1, [2, 7, 8], [3, 4], [5, [6, 7]]];
-console.log(squareArrayElements(arr));
+const arr = [1, 4, 9, 16, 25];
+const result = arr.map(Math.sqrt);
+console.log(result);
