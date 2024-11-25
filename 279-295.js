0e87d7c97b4f8baa330872dbd0f5c4a3d5a4cbbe
ray__s <Rayy_Bush@proton.me>
2024-11-26 01:06:02 +0300

    Задания 279-295

--- 279-295.js
+++ 279-295.js
@@ -1,5 +1,5 @@
-const arr = [2, 3, 5, 7, 8];
+const arr = [1, 2, 3, 4, 5];
 
-const allValid = arr.every((item, index) => item * (index + 1) < 30);
+const allPositive = arr.every(item => item > 0);
 
-console.log(allValid);
+console.log(allPositive);
