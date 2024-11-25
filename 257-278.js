c6264f4fb2bbee7a65a4e40d1588dd51be7d3cd9
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:50:03 +0300

    Задания 257-278

--- 257-278.js
+++ 257-278.js
@@ -1,12 +1,18 @@
 function countdown() {
     let number = 10;
     return function() {
-        number--;
-        console.log(number);
+        if (number > 0) {
+            number--;
+            console.log(number);
+        } else {
+            console.log("Отсчет окончен");
+        }
     };
 }
-
 const countdownFrom10 = countdown();
+countdownFrom10();
+countdownFrom10();
+countdownFrom10();
 countdownFrom10();
 countdownFrom10();
 countdownFrom10();
