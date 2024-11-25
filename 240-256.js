f13a9400ae23afe17c6810cd71fe9b9d72860f7c
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:45:25 +0300

    Задания 240-256

--- 240-256.js
+++ 240-256.js
@@ -1,10 +1,5 @@
-function each(arr, func) {
-    for (let i = 0; i < arr.length; i++) {
-        arr[i] = func(arr[i], i, arr);
-    }
-}
-let numbers = [1, 2, 3, 4, 5];
-each(numbers, function(num) {
-    return num * 2;
+let strings = ["hello", "world", "javascript"];
+each(strings, function(str) {
+    return str.split('').reverse().join('');
 });
-console.log(numbers);
+console.log(strings);
