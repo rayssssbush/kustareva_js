86481a0e71c1102094d4d79d019f1b28f5017339
ray__s <Rayy_Bush@proton.me>
2024-11-26 01:41:05 +0300

    Задания 311-336

--- 311-336.js
+++ 311-336.js
@@ -1,11 +1,3 @@
-function formatDate(date) {
-    let parts = date.split('-');
-    let year = parts[0];
-    let month = parts[1];
-    let day = parts[2];
-    
-    return `${day}.${month}.${year}`;
-}
-let inputDate = "2024-12-31";
-let formattedDate = formatDate(inputDate);
-console.log(formattedDate);
+let today = new Date();
+let dayOfWeek = today.getDay();
+console.log(dayOfWeek);
