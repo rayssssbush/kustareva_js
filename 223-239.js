9455609da489577df01c35483929824f22a43f24
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:36:17 +0300

    Задания 223-239

--- 223-239.js
+++ 223-239.js
@@ -1,3 +1,16 @@
-for (let key in obj) {
-    console.log(obj[key]());
-}
+const obj = {
+    sum: function(arr) {
+        return arr.reduce((acc, num) => acc + num, 0);
+    },
+    sumOfSquares: function(arr) {
+        return arr.reduce((acc, num) => acc + num ** 2, 0);
+    },
+    sumOfCubes: function(arr) {
+        return arr.reduce((acc, num) => acc + num ** 3, 0);
+    }
+};
+const numbers = [1, 2, 3];
+
+console.log(obj.sum(numbers));
+console.log(obj.sumOfSquares(numbers));
+console.log(obj.sumOfCubes(numbers));
