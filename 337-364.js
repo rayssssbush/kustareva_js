dafe0db662343e999ad8c4df8ed5c34a6518fce5
ray__s <Rayy_Bush@proton.me>
2024-11-26 02:33:36 +0300

    Задания 337-364

--- 337-364.js
+++ 337-364.js
@@ -1,3 +1,2 @@
-let firstParagraph = document.querySelector('#block p');
-let firstParagraphFromClass = document.querySelector('.block p');
-let firstParagraphWithClass = document.querySelector('.www');
+document.getElementById('button1').addEventListener('click', func1);
+document.getElementById('button2').addEventListener('click', func2);
