50d6e959b4b71624939cab5e2a84368cfa705973
ray__s <Rayy_Bush@proton.me>
2024-11-26 01:45:10 +0300

    Задания 311-336

--- 311-336.js
+++ 311-336.js
@@ -1,3 +1,6 @@
-let date = new Date(2025, 0, 1);
-let timestamp = date.getTime();
-console.log(timestamp);
+let startDate = new Date(1988, 2, 1);
+let endDate = new Date(2000, 0, 10);
+let timeDifference = endDate - startDate;
+let daysDifference = timeDifference / (1000 * 3600 * 24);
+
+console.log(daysDifference);
