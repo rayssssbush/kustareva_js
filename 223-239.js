26768200718fa7305699ece177c2da7a82bfc322
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:34:31 +0300

    Задания 223-239

--- 223-239.js
+++ 223-239.js
@@ -1,8 +1,3 @@
-const obj = {
-    func1: function() { return 1; },
-    func2: function() { return 2; },
-    func3: function() { return 3; }
-};
-
-const sum = obj.func1() + obj.func2() + obj.func3();
-console.log(sum); // 6
+for (let key in obj) {
+    console.log(obj[key]());
+}
