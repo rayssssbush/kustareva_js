4c8238522a10acd49fd4b633cca6052fcf806ec5
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:39:13 +0300

    Задания 240-256

--- 240-256.js
+++ 240-256.js
@@ -1,3 +1,13 @@
-function test(func1, func2, func3) {
-    return func1() + func2() + func3();
+function func1() {
+    return 1;
 }
+
+function func2() {
+    return 2;
+}
+
+function func3() {
+    return 3;
+}
+
+console.log(test(func1, func2, func3));
