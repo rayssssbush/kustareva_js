60e3994bee76e6702cb6610f2d1a57ea58af9a3c
ray__s <Rayy_Bush@proton.me>
2024-11-26 01:46:37 +0300

    Задания 311-336

--- 311-336.js
+++ 311-336.js
@@ -1,6 +1,14 @@
-let startDate = new Date(1988, 2, 1);
-let endDate = new Date(2000, 0, 10);
-let timeDifference = endDate - startDate;
-let daysDifference = timeDifference / (1000 * 3600 * 24);
+let startDate = new Date(2000, 8, 1);
+let endDate = new Date(2010, 1, 15);
 
+let timeDifference = endDate - startDate;
+
+console.log(timeDifference);
+let daysDifference = timeDifference / (1000 * 3600 * 24);
 console.log(daysDifference);
+
+let monthsDifference = (endDate.getFullYear() - startDate.getFullYear()) * 12 + endDate.getMonth() - startDate.getMonth();
+console.log(monthsDifference);
+
+let yearsDifference = endDate.getFullYear() - startDate.getFullYear();
+console.log(yearsDifference);
