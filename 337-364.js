8049eda024fb1ad75e5f159afed76e71b5cb70e2
ray__s <Rayy_Bush@proton.me>
2024-11-26 02:31:18 +0300

    Задания 337-364

--- 337-364.js
+++ 337-364.js
@@ -1,7 +1,3 @@
-let elem1 = document.getElementById('elem1');
-let elem2 = document.getElementById('elem2');
-let elem3 = document.getElementById('elem3');
-
-console.log(elem1.textContent);
-console.log(elem2.textContent);
-console.log(elem3.textContent);
+let firstParagraph = document.querySelector('#block p');
+let firstParagraphFromClass = document.querySelector('.block p');
+let firstParagraphWithClass = document.querySelector('.www');
