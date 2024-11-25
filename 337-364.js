9765437febd047a537b35639eee63806a7663974
ray__s <Rayy_Bush@proton.me>
2024-11-26 02:34:12 +0300

    Задания 337-364

--- 337-364.js
+++ 337-364.js
@@ -1,5 +1,3 @@
-document.getElementById('elem1').addEventListener('click', func);
-document.getElementById('elem2').addEventListener('click', func);
-document.getElementById('elem3').addEventListener('click', func);
-document.getElementById('elem4').addEventListener('click', func);
-document.getElementById('elem5').addEventListener('click', func);
+document.getElementById('elem').addEventListener('click', func1);
+document.getElementById('elem').addEventListener('click', func2);
+document.getElementById('elem').addEventListener('click', func3);
