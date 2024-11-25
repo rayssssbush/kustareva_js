79bcb2b1847216149f2afc59c78f2b8e3d528a63
ray__s <Rayy_Bush@proton.me>
2024-11-26 00:34:21 +0300

    Задания 223-239

--- 223-239.js
+++ 223-239.js
@@ -1,11 +1,8 @@
-const arr = [
-    function() { return 1; },
-    function() { return 2; },
-    function() { return 3; }
-];
-console.log(arr[2]());
-const sum = arr[0]() + arr[1]() + arr[2]();
-console.log(sum);
-for (let func of arr) {
-    console.log(func());
-}
+const obj = {
+    func1: function() { return 1; },
+    func2: function() { return 2; },
+    func3: function() { return 3; }
+};
+
+const sum = obj.func1() + obj.func2() + obj.func3();
+console.log(sum); // 6
