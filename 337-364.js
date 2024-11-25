c37625d1d2fd028b87d0abb56ff994c87db5d06d
ray__s <Rayy_Bush@proton.me>
2024-11-26 02:28:51 +0300

    Задания 337-364

---
+++ 337-364.js
@@ -0,0 +1,7 @@
+let elem1 = document.getElementById('elem1');
+let elem2 = document.getElementById('elem2');
+let elem3 = document.getElementById('elem3');
+
+console.log(elem1.textContent);
+console.log(elem2.textContent);
+console.log(elem3.textContent);
