9f29b2e1a643888e23539977d9f668e6816272b3
ray__s <Rayy_Bush@proton.me>
2024-12-16 01:48:57 +0300

    Задания 464-484

--- 365-376.js
+++ 365-376.js
@@ -1,14 +1,26 @@
-let elems = document.querySelectorAll('li');
+let elems = document.querySelectorAll('#parent p');
 for (let elem of elems) {
-  elem.addEventListener('click', function func() {
+  let span = document.createElement('span');
+  span.textContent = elem.textContent;
+  elem.textContent = '';
+  elem.appendChild(span);
+  let removeLink = document.createElement('a');
+  removeLink.href = '';
+  removeLink.textContent = 'remove';
+  elem.appendChild(removeLink);
+  span.addEventListener('click', function func() {
     let input = document.createElement('input');
-    input.value = elem.textContent;
-    elem.textContent = '';
-    elem.appendChild(input);
+    input.value = span.textContent;
+    span.textContent = '';
+    span.appendChild(input);
     input.addEventListener('blur', function() {
-      elem.textContent = this.value;
-      elem.addEventListener('click', func);
-    }); 
-    elem.removeEventListener('click', func);
+      span.textContent = this.value;
+      span.addEventListener('click', func);
+    });
+    span.removeEventListener('click', func);
+  });
+  removeLink.addEventListener('click', function(event) {
+    event.preventDefault();
+    elem.remove();
   });
 }
