d05b500c4e7f8b2df244f47f5c3a9741dbe15483
ray__s <Rayy_Bush@proton.me>
2024-12-16 01:14:55 +0300

    Задания 464-484

--- 365-376.js
+++ 365-376.js
@@ -1,12 +1,10 @@
 const arr = ['Элемент 1', 'Элемент 2', 'Элемент 3'];
 const ul = document.getElementById('elem');
-
 arr.forEach(item => {
     const li = document.createElement('li');
     li.textContent = item;
     ul.appendChild(li);
-
     li.addEventListener('click', () => {
-        alert(item);
+        li.textContent += '!';
     });
 });
