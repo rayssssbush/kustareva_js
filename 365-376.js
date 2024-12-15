6d1d0daa5b6eb865233abc1c99a833b8d83d9aac
ray__s <Rayy_Bush@proton.me>
2024-12-16 02:00:19 +0300

    Задания 464-484

--- 365-376.js
+++ 365-376.js
@@ -1,17 +1,6 @@
-let rows = document.querySelectorAll('table tr');
-for (let row of rows) {
-  let link = document.createElement('a');
-  link.href = '#';
-  link.textContent = ' [highlight]';
-  let cell = document.createElement('td');
-  cell.appendChild(link);
-  row.appendChild(cell);
-  link.addEventListener('click', function(event) {
-    event.preventDefault();
-    if (row.style.backgroundColor === 'green') {
-      row.style.backgroundColor = '';
-    } else {
-      row.style.backgroundColor = 'green';
-    }
-  });
+function setAttr(selector, attr, value) {
+    let elem = document.querySelector(selector);
+    elem.setAttribute(attr, value);
 }
+setAttr('#elem1', 'class', 'newClass');
+setAttr('#elem2', 'title', 'New Title');
